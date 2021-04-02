c55d4492c914e4488ca2dc2d2a9ec855e3533b4e
razzledev <razzledev0@gmail.com>
2021-04-02 14:03:49 +0900

    Update 17.py

--- 17.py
+++ 17.py
@@ -1,36 +1,64 @@
-proper = [
-  0,
-  "one".length,
-  "two".length,
-  "three".length,
-  "four".length,
-  "five".length,
-  "six".length,
-  "seven".length,
-  "eight".length,
-  "nine".length,
-  "ten".length,
-  "eleven".length,
-  "twelve".length,
-  "thirteen".length,
-  "fourteen".length,
-  "fifteen".length,
-  "sixteen".length,
-  "seventeen".length,
-  "eighteen".length,
-  "nineteen".length
+wordy = [
+  "",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+  "twenty"
 ]
 
-tenth = [
-  "twenty".length,
-  "thirty".length,
-  "forty".length,
-  "fifty".length,
-  "sixty".length,
-  "seventy".length,
-  "eighty".length,
-  "ninety".length
+decadey = [
+  "",
+  "",
+  "twenty",
+  "thirty",
+  "forty", # 40의 정확한 spelling은 forty 이다... -> 처음에 fourty로 해서 답이 21224가 나왔었다...
+  "fifty",
+  "sixty",
+  "seventy",
+  "eighty",
+  "ninety"
 ]
 
-def oneTonine():
-    
+res = 0
+
+def euler():
+  global res
+  for x in range(1,20):
+    print(wordy[x], len(wordy[x]))
+    res += len(wordy[x])
+  for y in range(20,100):
+    wort = decadey[y//10] + wordy[y%10]
+    print(wort, len(wort))
+    res += len(wort)
+  for z in range(100, 1000):
+    if z % 100 == 0:
+      hundword = wordy[z//100] + "hundred"
+    elif int(str(z)[1:])//10 <= 1:
+      hundword = wordy[z//100] + "hundredand" + decadey[int(str(z)[1:])//10] + wordy[int(str(z)[1:])]
+    else:
+      hundword = wordy[z//100] + "hundredand" + decadey[int(str(z)[1:])//10] + wordy[int(str(z)[2])]
+    print(hundword, len(hundword))
+    res += len(hundword)
+  print("onethousand", len("onethousand"))
+  res += len("onethousand")
+  print("result : ", res)
+
+euler()
+
+# 21124
