0e98f61cb22351b990c1c13e2c2391f0756f5b33
razzledev <kingsparkdev@gmail.com>
2021-04-04 19:58:36 +0900

    Create 019.py

---
+++ 019.py
@@ -0,0 +1,31 @@
+thirtyone = [1,3,5,7,8,10,12]
+
+def findoutplus(a,b):
+        for each in thirtyone:
+            if each == b:
+                return 31
+        if a % 4 == 0 and a % 200 != 0:
+            if b == 2: return 29
+            else: return 30    
+        else:
+            if b == 2: return 28
+            else: return 30 
+
+def euler():
+    f_day = 7
+    res = 0
+
+    for year in range(1900,2000):
+        for month in range(1,13):
+            yM = findoutplus(year, month)
+            while f_day <= yM + 7:
+                if f_day > yM:
+                    f_day -= yM
+                    break
+                f_day += 7
+            if f_day == 1:
+                res += 1
+    
+    print(res)
+
+euler()
