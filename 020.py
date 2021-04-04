a990565d3d7a0101ef4daec9d7394deeb38542ac
razzledev <kingsparkdev@gmail.com>
2021-04-04 20:02:04 +0900

    Create 020.py

---
+++ 020.py
@@ -0,0 +1,13 @@
+def factorial_recursive(n):
+    return n * factorial_recursive(n-1) if n > 1 else 1
+
+def euler(k):
+    res = 0
+    res_arr = list(str(factorial_recursive(k)))
+
+    for each in res_arr:
+        res += int(each)
+
+    print(res)
+
+euler(100)
