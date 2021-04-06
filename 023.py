e6a3b6b1645c5e249e2c7f99f01b304b3659452f
razzledev <kingsparkdev@gmail.com>
2021-04-06 14:09:45 +0900

    2324

--- 023.py
+++ 023.py
@@ -1,22 +1,24 @@
-from utils import *
+import math
+import timeit
+
+start = timeit.default_timer()
 
 def euler(n):
-    res = 0
-    arr_adnum = abNum(n)
-    arr_succ = []
+    arr_abnum = abNum(n)
+    arr_succ = set(range(n))
+    a = 0
 
-    for a in range(len(arr_adnum)-1):
-        for b in range(len(arr_adnum)-1):
-            arr_succ.append(arr_adnum[a]+arr_adnum[b])
+    while a <= len(arr_abnum)-1:
+        b = 0
+        while b <= a and arr_abnum[a]+arr_abnum[b] < n:
+            try:
+                arr_succ.remove(arr_abnum[a]+arr_abnum[b])
+                b += 1
+            except:
+                b += 1
+        a += 1
 
-    arr_succ = list(set(arr_succ))
-    # arr_succ.sort()
-    
-    for l in range(1,n+1):
-        if l not in arr_succ:
-            res += l
-            
-    return res
+    return sum(arr_succ)
 
 def abNum(n):
     res = []
@@ -25,13 +27,27 @@ def abNum(n):
         flagN = 0
         divArr = nDivisors(k)
         divArr.remove(k)
-        for each in divArr:
-            flagN += int(each)
+        flagN = sum(divArr)
         if flagN > k:
             # 이 경우 과잉수라고 할 수 있다.
             res.append(k)
     return res
 
+def nDivisors(n):
+    res = []
+    # n의 제수를 반환 -> divisors(36) -> [1,2,3,4,6,9,12,18,36]
+    for p in range(1,math.ceil(math.sqrt(n))+1):
+        if n % p == 0:
+            res.append(int(p))
+            res.append(int(n/p))
+    res = list(set(res))
+    # res.sort()
+    return res
+
 print(euler(28123))
+
+stop = timeit.default_timer()
+
+print('Time: ', stop - start)
 
 # 4179871
